684f31d1e22b7aad8b026eca63f32d503ca164fc
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-02-05 17:30:55 -0300

    Update

--- src/components/SectionContact/SectionContact.jsx
+++ src/components/SectionContact/SectionContact.jsx
@@ -22,11 +22,7 @@ const SectionContact = () => {
       tl.current = gsap.timeline({
         scrollTrigger: {
           trigger: "#content-contact",
-<<<<<<< HEAD
           start: "-600px"
-=======
-          start: "-650px"
->>>>>>> 784b11dd1aff79ebb0694e909dedbfdf4106a510
         }
       })
 
