b28999c336c41cc803113c0693b2215ebd260221
Bruno Mendonça <brunomendoncamal@gmail.com>
2024-02-08 11:59:09 -0300

    Update Scroll Trigger About

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -20,6 +20,14 @@ export function About() {
           start: "top bottom"
         }
       })
+        .fromTo("#content-text", {
+          opacity: 0,
+          y: 100
+        }, {
+          opacity: 1,
+          y: 0,
+          duration: 1.3
+        })
         .fromTo("#content-banner", {
           opacity: 0,
           y: 200
@@ -28,7 +36,7 @@ export function About() {
           y: 0,
           duration: 1.3
         })
-        .fromTo("#content-text", {
+        .fromTo("#content-card", {
           opacity: 0,
           y: 200
         }, {
@@ -46,8 +54,8 @@ export function About() {
     <ContainerAboutPage>
       <ContentBanner></ContentBanner>
 
-      <ContentAbout>
-        <ContentAboutText>
+      <ContentAbout id='content-unit' ref={el}>
+        <ContentAboutText id='content-text'>
           <Title textTitle='Sobre Nós' />
           <Paragraph textParagraph='No dia 10 de Novembro de 2015, inaugurou a primeira unidade no Setor Marista em Goiânia.' />
           <Paragraph textParagraph='Inspirados pela tradição e determinados a criar um espaço onde os clientes pudessem experimentar mais do que um simples corte de cabelo, estabelecemos nossa barbearia como um refúgio de estilo e autenticidade. Ao longo dos anos, cultivamos uma reputação sólida, baseada na habilidade técnica, atenção aos detalhes e atendimento ao cliente personalizado.' />
@@ -55,11 +63,11 @@ export function About() {
           <Paragraph textParagraph='Cada corte de cabelo é uma expressão única de habilidade artística, refletindo não apenas a dedicação de nossos barbeiros, mas também a história rica que nos trouxe até aqui. Convidamos você a fazer parte desta jornada, onde a excelência é uma tradição, e cada cliente é uma parte vital da nossa história de sucesso.' />
         </ContentAboutText>
         <SecondCaption textSecondCaption='Conheça a nossa unidade' />
-        <ContentAboutUnit id='content-unit' ref={el}>
+        <ContentAboutUnit >
           <UnitBanner id='content-banner'>
             <img src="/banner-principal.jpg" alt="" />
           </UnitBanner>
-          <UnitText id='content-text'>
+          <UnitText id='content-card'>
             <SecondCaption textSecondCaption='Marista' />
             <Paragraph textParagraph='Localizada em um dos bairros mais tradicionais de Goiânia' />
             <Paragraph textParagraph='Convidamos você a fazer parte desta jornada, onde a excelência é uma tradição, e cada cliente é uma parte vital da nossa história de sucesso.' />
